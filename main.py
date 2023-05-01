f847cb4e832dc2f3864427d12531fa93091a5848
unknown <ayush02av@gmail.com>
2023-05-01 09:56:26 +0530

    Host

--- main.py
+++ main.py
@@ -88,4 +88,4 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         manager.disconnect(websocket)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=80, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
